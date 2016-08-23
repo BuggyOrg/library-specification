85b9882cfad6d97950935b91b5c6404cf1a23b08
leMaik <an@maik.tk>
2016-08-23 18:30:01 +0200

    (refactor) Make config tests asynchronous.

--- src/tests/config.js
+++ src/tests/config.js
@@ -12,8 +12,11 @@ export default function (serve) {
 
   describe('Configuration', () => {
     it('get configuration settings', () => {
-      return chai.request(serve({config: {c: 2}}))
-        .get('/config/c')
+      return serve({config: {c: 2}})
+        .then((app) =>
+          chai.request(app)
+          .get('/config/c')
+        )
         .then((res) => {
           expect(res.status).to.equal(200)
           expect(res.body).to.equal(2)
@@ -21,44 +24,41 @@ export default function (serve) {
     })
 
     it('sets configuration settings', () => {
-      var app = serve({config: {}})
-      return chai.request(app)
-        .post('/config/d')
-        .send({value: 4})
-        .then((res) => {
-          expect(res.status).to.equal(204)
-        })
-        .then(() => chai.request(app).get('/config/d'))
-        .then((res) => {
-          expect(res.status).to.equal(200)
-          expect(res.body).to.equal(4)
+      return serve({config: {}})
+        .then((app) => {
+          return chai.request(app)
+            .post('/config/d')
+            .send({value: 4})
+            .then((res) => {
+              expect(res.status).to.equal(204)
+            })
+            .then(() => chai.request(app).get('/config/d'))
+            .then((res) => {
+              expect(res.status).to.equal(200)
+              expect(res.body).to.equal(4)
+            })
         })
     })
 
     it('errors if the post value is invalid', () => {
-      var app = serve({config: {}})
-      return expect(chai.request(app)
-        .post('/config/d')
-        .send({X: 4})
-        .then((res) => {
-        })).to.be.rejected
+      return serve({config: {}})
+        .then((app) => {
+          return expect(chai.request(app)
+            .post('/config/d')
+            .send({X: 4})
+            .then((res) => {
+            })).to.be.rejected
+        })
     })
 
     it('errors if the config is not defined', () => {
-      var app = serve({config: {}})
-      return expect(chai.request(app)
-        .get('/config/r')
-        .then((res) => {
-        })).to.be.rejected
-    })
-
-    it('can export the complete DB', () => {
-      return chai.request(serve({Components: [], meta: {}, config: {a: 1}}))
-      .get('/export')
-      .then((res) => {
-        expect(res.status).to.equal(200)
-        expect(res.body).to.eql({Components: [], meta: {}, config: {a: 1}})
-      })
+      return serve({config: {}})
+        .then((app) => {
+          return expect(chai.request(app)
+            .get('/config/r')
+            .then((res) => {
+            })).to.be.rejected
+        })
     })
   })
 }
